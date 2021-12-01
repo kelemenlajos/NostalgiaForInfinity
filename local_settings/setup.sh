f42eeb36835f7a01e684d133e1fb5e9014181b55
kelemenlajos <kelemenlajos@gmail.com>
2021-12-01 09:16:43 +0100

    Create setup.sh

---
+++ local_settings/setup.sh
@@ -0,0 +1,5 @@
+cd ~/NostalgiaForInfinity
+cp local_settings/docker-compose.yml .
+cp local_settings/Dockerfile.custom ./docker/
+cp local_settings/config.private.json ./user_data/
+echo 'config.secret.json-t másold be a user databa-ba!!!!'
