2ccc5302e97bf92ce21302ecb9af1d336549b9ff
kelemenlajos <kelemenlajos@gmail.com>
2022-01-26 17:18:54 +0100

    Update setup.sh

--- local_settings/setup.sh
+++ local_settings/setup.sh
@@ -2,4 +2,5 @@ cd ~/NostalgiaForInfinity
 cp local_settings/docker-compose.yml .
 cp local_settings/Dockerfile.custom ./docker/
 cp local_settings/config.private.json ./user_data/
+cp local_settings/nfi-hold-trades.json ./user_data/
 echo 'config.secret.json-t másold be a user databa-ba!!!!'
